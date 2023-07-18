6347254baff638c6a0987be138d4a94b19a7d1a5
Your Name <you@example.com>
2023-07-18 16:29:53 +0900

    Class Mysql

--- request.py
+++ request.py
@@ -2,5 +2,5 @@ import requests
 
 
 
-response = requests.get() https://api.openweathermap.org/data/2.5/weather?q=강서구&appid=3dc06380d440e4afeac98d7f604cbeb4
+response = requests.get(https://api.openweathermap.org/data/2.5/weather?q=강서구&appid=3dc06380d440e4afeac98d7f604cbeb4) 
 print(response.text)
